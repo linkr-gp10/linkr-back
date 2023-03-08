f17d5f89f0c6cc7aa92f811c036c508b51fb3ae4
lujolfs <lucas.j.frank@gmail.com>
2023-03-07 23:01:05 -0300

    chore: adjust code for deploy on render

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,12 @@
-import pg from "pg";
+import pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const {Pool} = pg;
+const {Pool} = pkg;
 
 const configDatabase = {
     connectionString: process.env.DATABASE_URL,
+    ssl: true
 };
 
 export const db = new Pool (configDatabase);
--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use(cors());
 app.use(userRouter);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
