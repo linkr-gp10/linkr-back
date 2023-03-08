ccd77847cb3407fa3ec6d81f0b51fd5835aba70b
lujolfs <lucas.j.frank@gmail.com>
2023-03-08 02:07:59 -0300

    grunt: add placeholder for redirect.

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -4,7 +4,8 @@ export async function signUp (req, res) {
 
     try {
         const result = await signupRepository(req.userObject);
-        res.redirect("/");
+        /* res.redirect("/"); */
+        res.send("Deu bom.")
     } catch (error) {
         res.status(500).send(error.message);
     }
